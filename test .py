4821f77d81a741b8cb95c78f8e6b10f8bd68357f
Kirill Dvornikov <kirill_2013_02_04@mail.ru>
2025-02-19 21:59:14 +0700

    добавил меню

--- test .py
+++ test .py
@@ -8,6 +8,7 @@ r = LifoQueue()
 gsp = deque()
 i=0
 i2=0
+RF=[]
 
 
 
@@ -83,274 +84,274 @@ global axiom
 global rules
 global iterations
 global angle
+p=0
 
-#Снежинка Коха
+name="Снежинка Коха"
 axiom = "F--F--F"
 rules = [["F","F+F--F+F"]]
 iterations = 4 # TOP, 7
 angle = 60
+RF.append([name,axiom,rules,iterations,angle])
+
+
+name="Квадратный остров Коха"
+axiom = "F+F+F+F"
+rules = [["F","F-F+F+FFF-F-F+F"]]
+iterations = 2 # TOP, 4
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name="Кристалл"
+axiom = "F+F+F+F"
+rules = [["F","FF+F++F+F"]]
+iterations = 3 # TOP, 6
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name="Квадратная снежинка"
+axiom = "F--F"
+rules = [["F","F-F+F+F-F"]]
+iterations = 4 # TOP, 6
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+
+name=" Фрактал Вичека"
+axiom = "F-F-F-F"
+rules = [["F","F-F+F+F-F"]]
+iterations = 4 # TOP, 6
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Кривая Леви"
+
+axiom = "F"
+rules = [["F","+F--F+"]]
+iterations = 10
+angle = 45
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Ковер Серпинского "
+
+axiom = "YF"
+rules = [["X","YF+XF+Y"],[ "Y","XF-YF-X"]]
+iterations = 7 
+angle = 60
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Решетка Серпинского "
+
+axiom = "FXF--FF--FF"
+rules = [["F","FF"],[ "X","--FXF++FXF++FXF--"]]
+iterations = 7 
+angle = 60
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Квадрат"
+
+axiom = "F+F+F+F"
+rules = [["F","FF+F+F+F+FF"]]
+iterations = 3 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Плитки"
+
+axiom = "F+F+F+F"
+rules = [["F","FF+F-F+F+FF"]]
+iterations = 3 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Кольца"
+
+axiom = "F+F+F+F"
+rules = [["F","FF+F+F+F+F+F-F"]]
+iterations = 2 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Крест-2"
+
+axiom = "F+F+F+F"
+rules = [["F","F+F-F+F+F"]]
+iterations = 3 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Pentaplexity"
+
+axiom = "F++F++F++F++F"
+rules = [["F","F++F++F+++++F-F++F"]]
+iterations = 3
+angle = 36
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" 32-сегментная кривая"
+
+axiom = "F+F+F+F"
+rules = [["F","-F+F-F-F+F+FF-F+F+FF+F-F-FF+FF-FF+F+F-FF-F-F+FF-F-F+F+F-F+"]]
+iterations = 3 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
 
+name=" Кривая Пеано-Госпера "
+
+axiom = "FX"
+rules = [["X","X+YF++YF-FX--FXFX-YF+"],[ "Y","-FX+YFYF++YF+FX--FX-Y"]]
+iterations = 4 
+angle = 60
+RF.append([name,axiom,rules,iterations,angle])
 
+name=" Кривая Серпинского"
+
+axiom = "F+XF+F+XF"
+rules = [["X","XF-F+F-XF+F+XF-F+F-X"]]
+iterations = 4
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
 
-# Квадратный остров Коха
-# axiom = "F+F+F+F"
-# rules = [["F","F-F+F+FFF-F-F+F"]]
-# iterations = 2 # TOP, 4
-# angle = 90
+name=" Анклеты Кришны"
+
+axiom = " -X--X"
+rules = [["X","XFX--XFX"]]
+iterations = 5 
+angle = 45
+RF.append([name,axiom,rules,iterations,angle])
 
+name=" Квадратный фрактал Госпера"
+
+axiom = "YF"
+rules = [["X", "XFX-YF-YF+FX+FX-YF-YFFX+YF+FXFXYF-FX+YF+FXFX+YF-FXYF-YF-FX+FX+YFYF-"], 
+        ["Y", "+FXFX-YF-YF+FX+FXYF+FX-YFYF-FX-YF+FXYFYF-FX-YFFX+FX+YF-YF-FX+FX+YFY"]]
+iterations = 2 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
 
-# Кристалл
+name=" Кривая Мура"
+
+axiom = "LFL-F-LFL"
+rules = [["L","+RF-LFL-FR+"], ["R","-LF+RFR+FL-"]]
+iterations = 4 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
 
-# axiom = "F+F+F+F"
-# rules = [["F","FF+F++F+F"]]
-# iterations = 3 # TOP, 6
-# angle = 90
-
-
-# Квадратная снежинка
-
-# axiom = "F--F"
-# rules = [["F","F-F+F+F-F"]]
-# iterations = 4 # TOP, 6
-# angle = 90
-
-
-# Фрактал Вичека
-
-# axiom = "F-F-F-F"
-# rules = [["F","F-F+F+F-F"]]
-# iterations = 4 # TOP, 6
-# angle = 90
-
-
-# Кривая Леви
-
-# axiom = "F"
-# rules = [["F","+F--F+"]]
-# iterations = 10 # TOP, 16
-# angle = 45
-
-
-# Ковер Серпинского ❤❤❤
-
-# axiom = "YF"
-# rules = [["X","YF+XF+Y"],[ "Y","XF-YF-X"]]
-# iterations = 7 # TOP, 10
-# angle = 60
-
-
-# Решетка Серпинского ❤❤❤
-
-# axiom = "FXF--FF--FF"
-# rules = [["F","FF"],[ "X","--FXF++FXF++FXF--"]]
-# iterations = 7 # TOP, 8
-# angle = 60
-
-
-# Квадрат
-
-# axiom = "F+F+F+F"
-# rules = [["F","FF+F+F+F+FF"]]
-# iterations = 3 # TOP, 5
-# angle = 90
-
-
-# Плитки
-
-# axiom = "F+F+F+F"
-# rules = [["F","FF+F-F+F+FF"]]
-# iterations = 3 # TOP, 4
-# angle = 90
-
-
-# Кольца
-
-# axiom = "F+F+F+F"
-# rules = [["F","FF+F+F+F+F+F-F"]]
-# iterations = 2 # TOP, 4
-# angle = 90
-
-
-# Крест-2
-
-# axiom = "F+F+F+F"
-# rules = [["F","F+F-F+F+F"]]
-# iterations = 3 # TOP, 6
-# angle = 90
-
-
-# Pentaplexity
-
-# axiom = "F++F++F++F++F"
-# rules = [["F","F++F++F+++++F-F++F"]]
-# iterations = 1 # TOP, 5
-# angle = 36
-
-
-# 32-сегментная кривая
-
-# axiom = "F+F+F+F"
-# rules = [["F","-F+F-F-F+F+FF-F+F+FF+F-F-FF+FF-FF+F+F-FF-F-F+FF-F-F+F+F-F+"]]
-# iterations = 3 # TOP, 3
-# angle = 90
-
-
-# Кривая Пеано-Госпера ❤❤❤
-
-# axiom = "FX"
-# rules = [["X","X+YF++YF-FX--FXFX-YF+"],[ "Y","-FX+YFYF++YF+FX--FX-Y"]]
-# iterations = 4 # TOP, 6
-# angle = 60
-
-
-# Кривая Серпинского
-
-# axiom = "F+XF+F+XF"
-# rules = [["X","XF-F+F-XF+F+XF-F+F-X"]]
-# iterations = 4 # TOP, 8
-# angle = 90
-
-
-# Анклеты Кришны
-
-# axiom = " -X--X"
-# rules = [["X","XFX--XFX"]]
-# iterations = 5 # TOP, 9
-# angle = 45
-
-
-# Квадратный фрактал Госпера
-
-# axiom = "YF"
-# rules = [["X", "XFX-YF-YF+FX+FX-YF-YFFX+YF+FXFXYF-FX+YF+FXFX+YF-FXYF-YF-FX+FX+YFYF-"], 
-#         ["Y", "+FXFX-YF-YF+FX+FXYF+FX-YFYF-FX-YF+FXYFYF-FX-YFFX+FX+YF-YF-FX+FX+YFY"]]
-# iterations = 2 # TOP, 3
-# angle = 90
-
-
-# Кривая Мура
-
-# axiom = "LFL-F-LFL"
-# rules = [["L","+RF-LFL-FR+"], ["R","-LF+RFR+FL-"]]
-# iterations = 4 # TOP, 8
-# angle = 90
-
-
-# Кривая Гильберта
-
-# axiom = "L"
-# rules = [["L","+RF-LFL-FR+"], ["R","-LF+RFR+FL-"]]
-# iterations = 4 # TOP, 9
-# angle = 90
-
-
-# Кривая Гильберта-II
-
-# axiom = "X"
-# rules = [["X","XFYFX+F+YFXFY-F-XFYFX"],[ "Y" ,"YFXFY-F-XFYFX+F+YFXFY"]]
-# iterations = 3 # TOP, 6
-# angle = 90
-
-
-# Кривая Пеано
-
-# axiom = "F"
-# rules = [["F","F+F-F-F-F+F+F+F-F"]]
-# iterations = 3 # TOP, 5
-# angle = 90
-
-
-# Крест
-
-# axiom = "F+F+F+F"
-# rules = [["F","F+FF++F+F"]]
-# iterations = 3 # TOP, 6
-# angle = 90
-
-
-# Треугольник
-
-# axiom = "F+F+F"
-# rules = [["F","F-F+F"]]
-# iterations = 4 # TOP, 9
-# angle = 120
-
-
-# Кривая дракона
-
-# axiom = "FX"
-# rules = [["X","X+YF+"],[ "Y","-FX-Y"]]
-# iterations = 8 # TOP, 16
-# angle = 90
-
-
-# Кривая Terdragon
-
-# axiom = "F"
-# rules = [["F","F-F+F"]]
-# iterations = 7 # TOP, 10
-# angle = 120
-
-
-# Двойная кривая дракона
-
-# axiom = "FX+FX"
-# rules = [["X","X+YF+"],["Y","-FX-Y"]]
-# iterations = 6 # TOP, 16
-# angle = 90
-
-
-# Тройная кривая дракона
-
-# axiom = "FX+FX+FX"
-# rules = [["X","X+YF+"], ["Y","-FX-Y"]]
-# iterations = 7 # TOP, 15
-# angle = 90   
-
-# t.left(90)
-# axiom = "F"
-# rules = [["F","FF-[-F+F+F]+[+F-F-F]"]]
-# iterations = 3 # TOP, 15
-# angle = 22
-
-
-# axiom = "X"
-# rules = [["F","F"],["X","[-F+F[Y]+F][+F-F[X]-F]"],["Y","[-F+F[Y+F][+F-F-F]]"]]
-# iterations = 100 # TOP, 15
-# angle = 60
-
-# t.right(-90)
-# angle = 30
-# def yaxis(size, lvl):   
-#     if lvl > 0:
-#         t.forward(size)
-#         t.right(angle)
-#         yaxis(0.8 * size, lvl-1)
-#         t.lt( 2 * angle )
-#         yaxis(0.8 * size, lvl-1)
-#         t.right(angle)
-#         t.forward(-size)
-# yaxis(80, 7)
-
-#апериодическая мозаика ❤❤❤
-
-# axiom = "X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X"
-# rules = [["F","F"],["X","[F+F+F+F[--X-Y]+++++F++++++++F-F-F-F]"]]
-# iterations = 20 # TOP, 15
-# angle = 15
-
-
-
-
-
-
-
-
-
-
-
-
-
+name=" Кривая Гильберта"
+
+axiom = "L"
+rules = [["L","+RF-LFL-FR+"], ["R","-LF+RFR+FL-"]]
+iterations = 4 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Кривая Гильберта-II"
+
+axiom = "X"
+rules = [["X","XFYFX+F+YFXFY-F-XFYFX"],[ "Y" ,"YFXFY-F-XFYFX+F+YFXFY"]]
+iterations = 3 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Кривая Пеано"
+
+axiom = "F"
+rules = [["F","F+F-F-F-F+F+F+F-F"]]
+iterations = 3 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Крест"
+
+axiom = "F+F+F+F"
+rules = [["F","F+FF++F+F"]]
+iterations = 3 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Треугольник"
+
+axiom = "F+F+F"
+rules = [["F","F-F+F"]]
+iterations = 4 
+angle = 120
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Кривая дракона"
+
+axiom = "FX"
+rules = [["X","X+YF+"],[ "Y","-FX-Y"]]
+iterations = 8 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Кривая Terdragon"
+
+axiom = "F"
+rules = [["F","F-F+F"]]
+iterations = 7 
+angle = 120
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Двойная кривая дракона"
+
+axiom = "FX+FX"
+rules = [["X","X+YF+"],["Y","-FX-Y"]]
+iterations = 6 
+angle = 90
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Тройная кривая дракона"
+
+axiom = "FX+FX+FX"
+rules = [["X","X+YF+"], ["Y","-FX-Y"]]
+iterations = 4
+angle = 90   
+RF.append([name,axiom,rules,iterations,angle])
+
+name="апериодическая мозаика "
+
+axiom = "X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X+X"
+rules = [["F","F"],["X","[F+F+F+F[--X-Y]+++++F++++++++F-F-F-F]"]]
+iterations=20
+angle = 15
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Фрактал 1"
+
+t.left(90)
+axiom = "F"
+rules = [["F","FF-[-F+F+F]+[+F-F-F]"]]
+iterations = 3 # TOP, 15
+angle = 22
+RF.append([name,axiom,rules,iterations,angle])
+
+name=" Фрактал 2"
+
+axiom = "X"
+rules = [["F","F"],["X","[-F+F[Y]+F][+F-F[X]-F]"],["Y","[-F+F[Y+F][+F-F-F]]"]]
+iterations = 100 # TOP, 15
+angle = 60
+RF.append([name,axiom,rules,iterations,angle])
+
+#[name,axiom,rules,iterations,angle]
+
+for i in range (len(RF)) :
+    print(i," - ", RF[i][0])
+print("Введите номер понравившегося фрактала из списка ")
+f=int(input())
+print("Введите итирацию или введите 0 чтобы пропустить этот шаг")
+p=int(input())
+if p > 0:
+    iterations=p
+else:
+     iterations=RF[f][3]
+name=RF[f][0]
+axiom=RF[f][1]
+rules=RF[f][2]
+angle=RF[f][4]
+
+
+print ("Фрактал",name)
 mainstring=axiom
 print ("begin")
 print (mainstring)
